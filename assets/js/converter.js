9c365974ff738a768098f85a9af9dbe5bc840c1c
David CR <alu0100536652@ull.edu.es>
2015-10-20 00:11:16 +0000

    Primeros test y clase Medida, Temperatura, Distancia, Celsius y Farenheit

--- assets/js/converter.js
+++ assets/js/converter.js
@@ -1,4 +1,41 @@
 (function(exports) {
     "use strict";
     
+    function Measure(value,type) {
+        this.value = value || 0;
+        this.type = type || "Undefined";
+    }
+    
+    function Temperature(value,type) {
+        Measure.call(this,value,type);
+    }
+    
+    function Distance(value,type) {
+        Measure.call(this,value,type);
+    }
+    
+    function Celsius(value) {
+        this.tipo = 'c';
+        Temperature.call(this, value, this.type);
+        this.toFarenheit = function()
+        {
+            return new Farenheit((this.valor * 9/5) + 32);
+        }
+    }
+    
+    function Farenheit(value) {
+        this.tipo = 'f';
+        Temperature.call(this, value, this.type);
+        this.toCelsius = function()
+        {
+            return new Celsius((this.valor - 32)  * (5/9));
+        }
+    }
+    
+    exports.Measure = Measure;
+    exports.Temperature = Temperature;
+    exports.Celsius = Celsius;
+    exports.Farenheit = Farenheit;
+    exports.Distance = Distance;
+    
 })(this);
---
+++ test/tests.js
@@ -0,0 +1,53 @@
+var expect = chai.expect,
+    should = chai.should();
+
+describe("TEST: Converter", function() {
+  
+  var sandbox;
+
+  beforeEach(function() {
+    // create a sandbox
+    sandbox = sinon.sandbox.create();
+
+    // stub some console methods
+    sandbox.stub(window.console, "log");
+    sandbox.stub(window.console, "error");
+  });
+
+  afterEach(function() {
+    // restore the environment as it was before
+    sandbox.restore();
+  });
+    
+  describe("Constructor", function() {
+      it("Measure instance - Default value of Measure",function() {
+          var measure = new Measure();
+          expect(measure.value).to.equal(0);
+      });
+      
+      it("Temperature instance - Value of Temperature is 12C",function() {
+          var temperature = new Temperature(12,"C");
+          expect(temperature.value).to.equal(12);
+      });
+      
+      it("Distance instance - Type of distance should be String", function() {
+          var distance = new Distance(3,"M");
+          expect(distance.type).to.be.a('string');
+      });
+      
+      it("Celsius instance - Value of Temperature is 0C",function() {
+          var celsius = new Celsius(0);
+          expect(celsius.value).to.equal(0);
+      });
+      
+      it("Farenheit instance - Type of Farenheit should be Number", function() {
+          var farenheit = new Farenheit(32);
+          expect(farenheit.value).to.be.a('number');
+      });
+  });
+  
+  describe("Methods", function() {
+      
+  })
+  
+});
